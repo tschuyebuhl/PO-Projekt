4cdc8cffe7d9b0a26229d7cc8f02e475520e905b
Ksawery Wróbel <ksawery.wrobel@tecna.pl>
2021-12-17 22:46:25 +0100

    asdas

---
+++ Source1.cpp
@@ -0,0 +1,33 @@
+/* 
+Projekt
+
+Wykonać implementacje programu symulującego działanie apteki.
+
+Należy pamiętać że apteka nie jest zwykłym sklepem, część leków jest wydawana tylko na receptę (wystawioną przez lekarza), a każdy zakup jest rejestrowany i archiwizowany.
+
+Zaprezentować w programie wszystkie mechanizmy i paradygmaty poznane na zajęciach:
+
+-Własne typy (typedef)
+
+-Klasy, Konstruktory, destruktory
+
+-Budowanie hierarchii klas, Budowanie klas z innych klas
+
+-Hermetyzazja, Dziedziczenie, Polimorfizm, funkcje wirtualne, bazowe klasy abstrakcyjne
+
+-Zaprzyjaźnianie funkcji i klas
+
+-Przeciążanie operatorów
+
+-Szablony funkcji, szablony klas
+
+Dla chętnych: obsługa plików.
+
+
+
+Przygotować sprawozdanie zawierające opis działania programu, przykładowe (najważniejsze/najciekawsze) fragmenty implementacji (kod z objaśnieniem) oraz diagram klas w notacji UML
+
+ */
+
+
+
